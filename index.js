9625c99f00c0deb2bb347c1a096b2952a3a0221e
Sun Lee <sunlee.newyork@gmail.com>
2016-09-04 09:38:43 -0400

    Pushing all changes to remote before selling MBA

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 loadOlapicSDK = function(){
 
-    devkit = new OlapicDevKit();
-
     var mediaList = $('.body .container .item-list'),
-        streamsNavList = $('.subnav.streams > ul');
-
+        streamsNavList = $('.subnav.streams > ul'),
+        devkit = new OlapicDevKit();
+        
     devkit.connectWithToken('459650359055b2722dbd12888d7cb74dc9793c2a71ed644d8203229d3fe1374f')
         .then(function(customer){
-            var batch = new devkit.api.mediaBatch(customer, 'photorank', 20);
+            var batch = new devkit.api.mediaBatch(customer, 'photorank', 50);
             batch.fetch()
                 .then(function (media){
                     $.each(media, function (i, e) {
@@ -29,7 +28,47 @@ loadOlapicSDK = function(){
                 });    
             // need to make direct API call here - no devkit function available
             // "//z3photorankapi-a.akamaihd.net/customers/216960/streams/search"
-            // then FOR EACH stream result, make following call:          
+            // then FOR EACH stream result, make following call:   
+            $.ajax({
+                url: 'http://rest.photorank.me/customers/216960/streams?auth_token=459650359055b2722dbd12888d7cb74dc9793c2a71ed644d8203229d3fe1374f&version=v2.2', 
+                method: 'GET'
+            }).success(function (result) {
+                console.log(result.data._embedded.gallery);
+                $.each(result.data._embedded.gallery, function(i, e) {
+                    streamsNavList.append('<li class="filter">'+e.name+'</li>'); 
+                });                
+                $('.subnav.streams > ul > li').click(function (r) {
+                    $('.item-list').html("");
+                    var filter =  r.currentTarget.innerText;
+                    console.log('Filter: ', filter);
+                    customer.searchStream(filter)
+                        .then(function (stream) {
+                            var streamBatch = new devkit.api.mediaBatch(stream, 'photorank', 50);
+                            streamBatch.fetch()
+                                .then(function (media){
+                                    $.each(media, function (i, e) {
+                                        mediaList.append('<li class="item"><span>'+e.get('images/original')+'</span><img src="'+e.get('images/mobile')+'" /></li>');
+                                    });
+                                    $(window).scroll(function() {   
+                                        if($(window).scrollTop() + $(window).height() > $(document).height() - 1) {
+                                            batch.next()
+                                            .then(function (media){
+                                                $.each(media, function (i, e) {                            
+                                                    mediaList.append('<li class="item"><span>'+e.get('images/original')+'</span><img src="'+e.get('images/mobile')+'" /></li>');
+                                                });              
+                                            });
+                                        }
+                                    });
+                                })
+                                .fail(function (error){
+                                    console.log('Initial media batch error: ', error);
+                                });    
+                        })
+                        .fail(function (error) {
+                            console.log('Stream search error: ', error);
+                        });
+                });
+            });                   
             $('.subnav.sorting > ul > li').click(function (r) {         
                 $('.item-list').html("");
                 var filter =  r.currentTarget.innerText;
@@ -55,45 +94,7 @@ loadOlapicSDK = function(){
                         console.log('Initial media batch error: ', error);
                     });    
             });
-            $.ajax({
-                url: 'http://rest.photorank.me/customers/216960/streams?auth_token=459650359055b2722dbd12888d7cb74dc9793c2a71ed644d8203229d3fe1374f&version=v2.2', 
-                method: 'GET'
-            }).success(function (result) {
-                $.each(result.data._embedded.gallery, function(i, e) {
-                    streamsNavList.append('<li class="filter">'+e.name+'</li>'); 
-                });                
-                $('.subnav.streams > ul > li').click(function (r) {
-                    $('.item-list').html("");
-                    var filter =  r.currentTarget.innerText;
-                    console.log('Filter: ', filter);
-                    customer.searchStream('Lemur')
-                        .then(function (stream) {
-                            var streamBatch = new devkit.api.mediaBatch(stream, 'photorank', 20);
-                            streamBatch.fetch()
-                                .then(function (media){
-                                    $.each(media, function (i, e) {
-                                        mediaList.append('<li class="item"><span>'+e.get('images/original')+'</span><img src="'+e.get('images/mobile')+'" /></li>');
-                                    });
-                                    $(window).scroll(function() {   
-                                        if($(window).scrollTop() + $(window).height() > $(document).height() - 1) {
-                                            batch.next()
-                                            .then(function (media){
-                                                $.each(media, function (i, e) {                            
-                                                    mediaList.append('<li class="item"><span>'+e.get('images/original')+'</span><img src="'+e.get('images/mobile')+'" /></li>');
-                                                });              
-                                            });
-                                        }
-                                    });
-                                })
-                                .fail(function (error){
-                                    console.log('Initial media batch error: ', error);
-                                });    
-                        })
-                        .fail(function (error) {
-                            console.log('Stream search error: ', error);
-                        });
-                });
-            });                    
+                    
         })
         .fail(function(error){
             console.log('Customer error: ', error);
